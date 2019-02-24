d4b588b3e1d17ca0f4545f695f165ae284d78a8a
wxie <wxie11@hawk.iit.edu>
2019-02-24 13:27:03 -0600

    run foregin key migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_192227) do
+ActiveRecord::Schema.define(version: 2019_02_24_192455) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_02_24_192227) do
     t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
