24ce5cfaef1f71fd19597dffdf7c81f0ad1735dd
wxie <wxie11@hawk.iit.edu>
2019-02-24 13:23:43 -0600

    run restaurant migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_24_192022) do
+ActiveRecord::Schema.define(version: 2019_02_24_192227) do
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
     t.string "image_url"
     t.string "cuisine_type"
     t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
