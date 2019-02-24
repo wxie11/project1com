30d4b15e46d68fdc26e68cebd912dabf47d0c9a8
wxie <wxie11@hawk.iit.edu>
2019-02-24 13:38:52 -0600

    add simple validation

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,8 @@
 class Dish < ApplicationRecord
   belongs_to :restaurant
+  validates :name, :image_url, :price, :cuisine_type, :restaurant, presence: true
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(jpg|png)\Z}i,
+    message: 'Image must be in either JPG or PNG format.'}
 end
