638c7084f545e315248e4d4a4fbdf2c046f85468
wxie <wxie11@hawk.iit.edu>
2019-03-12 18:15:35 -0500

    add dish validation and test

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,7 @@ class Dish < ApplicationRecord
   belongs_to :restaurant
   validates :name, :image_url, :price, :cuisine_type, :restaurant, presence: true
   validates :name, uniqueness: true
+  validates :price, allow_blank: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(jpg|png)\Z}i,
     message: 'Image must be in either JPG or PNG format.'}
