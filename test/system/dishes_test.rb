bc0763ec8aa7509950e4869ac0d3e3b210c0a3cd
wxie <wxie11@hawk.iit.edu>
2019-03-25 22:06:44 -0500

    update ruby

--- test/system/dishes_test.rb
+++ test/system/dishes_test.rb
@@ -2,38 +2,50 @@ require "application_system_test_case"
 
 class DishesTest < ApplicationSystemTestCase
   setup do
-    @dish = dishes(:one)
+    @dish = dishes(:veg_pasta)
   end
 
   test "visiting the index" do
     visit dishes_url
-    assert_selector "h1", text: "Dishes"
+    assert_selector "header h2", text: "Favorite Dishes"
   end
 
   test "creating a Dish" do
     visit dishes_url
     click_on "New Dish"
 
-    click_on "Create Dish"
+    fill_in "Name", with: @dish.name
+    fill_in "Image url", with: @dish.image_url
+    fill_in "Price", with: @dish.price
+    fill_in "Cuisine type", with: @dish.cuisine_type
+    fill_in "Restaurant", with: @dish.restaurant_name
+
+    click_on "Save"
 
     assert_text "Dish was successfully created"
     click_on "Back"
   end
 
   test "updating a Dish" do
-    visit dishes_url
+    visit dish_url(@dish)
     click_on "Edit", match: :first
 
-    click_on "Update Dish"
+    fill_in "Name", with: @dish.name
+    fill_in "Image url", with: @dish.image_url
+    fill_in "Price", with: @dish.price
+    fill_in "Cuisine type", with: @dish.cuisine_type
+    fill_in "Restaurant", with: @dish.restaurant_name
+
+    click_on "Save"
 
     assert_text "Dish was successfully updated"
     click_on "Back"
   end
 
   test "destroying a Dish" do
-    visit dishes_url
+    visit dish_url(@dish)
     page.accept_confirm do
-      click_on "Destroy", match: :first
+    click_on "Destroy", match: :first
     end
 
     assert_text "Dish was successfully destroyed"
